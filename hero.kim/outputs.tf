79a913074c574bc861424eddb377933fef80a6a9
EC2 Default User <ec2-user@ip-10-0-0-178.ap-northeast-2.compute.internal>
2024-09-22 08:01:03 +0000

    rds

--- hero.kim/outputs.tf
+++ hero.kim/outputs.tf
@@ -8,17 +8,7 @@ output "web_ip" {
   value       = module.ec2.web_ip
 }
 
-output "web1_ip" {
-  description = "The public IP of the Bastion host"
-  value       = module.ec2.web1_ip
-}
-
 output "was_ip" {
   description = "The public IP of the Bastion host"
   value       = module.ec2.was_ip
-}
-
-output "was1_ip" {
-  description = "The public IP of the Bastion host"
-  value       = module.ec2.was1_ip
 }
